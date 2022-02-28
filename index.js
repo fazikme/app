c1e83ca7bd1199059e8416aae3b813badb04894f
fazikme <fazikme@gmail.com>
2022-02-28 23:46:46 +0500

    index.js

--- index.js
+++ index.js
@@ -2,4 +2,38 @@
 
 const numberofnews = +prompt("Siz qancha yangilik ko'rdingiz?");
 
+const personalofnewsDB = {
+    count: numberofnews,
+    news: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+for(let i=0; i<2; i++) {
+    const a = prompt("Oxirgi ko'rgan yangiliklaringizdan biri?"),
+          b = prompt("unga neci baxo berasiz?");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalofnewsDB.news[a] = b;
+        console.log("tayyor");
+    }
+    else {
+        console.log("error")
+        i--;
+    }
+}
+
+if (personalofnewsDB.count < 10) {
+    console.log("Слишком меньше");
+} else if (personalofnewsDB.count >= 10 && personalofnewsDB.count <= 30) {
+    console.log("You are classic");
+} else if (personalofnewsDB.count >=30) {
+    console.log("Prosta Super");
+} else {
+    console.log("Error");
+}
+
+console.log(personalofnewsDB);
+
 
---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let x = 5;
+// the first method
+while (x <= 1000) {
+    console.log(x)
+    x++;
+}
+
+//the second method
+let x = 5;
+do {
+    console.log(x);
+    x++;
+} while(x>=10); 
